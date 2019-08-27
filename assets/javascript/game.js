2b90ddb4e9df638a5ad5c49fd0bebc35f1a2cd8b
AR-Lilypad <angie-for-seekers@msn.com>
2019-08-27 18:59:31 -0400

    fixes to js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,40 +3,48 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var guessesSoFar = [];
-var computerLetters = []
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var computerLetters;
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 var winsText = document.getElementById('wins-text');
 var lossesText = document.getElementById("losses-text");
 var guessesLeftText = document.getElementById("guessesleft-Text");
 var guessesSoFarText = document.getElementById("guessessofar-text");
+// console.log(computerLetters,"emptyarray");
 
 //Random letter generator for the computer choice, which needs to begin the guessing.
 computerLetters = letters[Math.floor(Math.random() * letters.length)];
-letters.push(computerLetters);
-console.log(computerLetters);
+console.log(computerLetters, "computer guess");
+console.log(letters, 'this is the letters array');
+
 
 
 // player chooses letters to answer computer choice
 document.onkeyup = function (event) {
     var playerLetters = event.key;
-    letters.push(playerLetters);
-    for(var i = 0; i < playerLetters.length; i++){
-    console.log(playerLetters.charAt(i));
-}
+    letters.push(playerLetters);   //push the letters to the guesses so far array  check mdn arrays - join?  or what.
+    for (var i = 0; i < playerLetters.length; i++) {
+        console.log(playerLetters.charAt(i));
+    }
     //test if playerLetters equals computerLetters, if true it increments wins by 1, if false player gets additional tries up to 10 times.  Display guesses so far.
     if (playerLetters === computerLetters) {
         wins = wins + 1;  // wins += 1
     } else if (playerLetters !== computerLetters) {
         guessesLeft--;
         guessesSoFar;
-    } else ((playerLetters !== computerLetters) && (guessesLeft === 0)) ;
+    } 
+    
+    if ((playerLetters !== computerLetters) && (guessesLeft === 0)) {
         losses++;
     }
-
-//function restart (this); {
-
+    winsText.textContent = "Wins:  " + wins;
+    lossesText.textContent = "Losses:  " + losses;
+    guessesLeftText.textContent = "Guesses Left:  " + guessesLeft;
+    guessesSoFarText.textContent = "Guesses So Far:  " + guessesSoFar;
+}
 winsText.textContent = "Wins:  " + wins;
 lossesText.textContent = "Losses:  " + losses;
 guessesLeftText.textContent = "Guesses Left:  " + guessesLeft;
 guessesSoFarText.textContent = "Guesses So Far:  " + guessesSoFar;
+
+//function restart (this); {
